95e3b9599b1ffc1a11a50b9a30917adf668865cc
PhoenixOleg <metall_2000@mail.ru>
2024-04-13 02:06:32 +0300

    Подключение EF Core, CSS для приложения

---
+++ SocialNetwork/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SocialNetwork.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+    //@@@ Тут должен быть город - его ID
+    }
+}
--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,14 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SocialNetwork.Controllers.Context;
+using SocialNetwork.Models;
+
 namespace SocialNetwork
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            // Создаем WebApplicationBuilder
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Получаем строку подключения из файла конфигурации appsettings.json
+            string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            #region Добавление сервисов в контейнер
+
+            // Добавляем контекст ApplicationDbContext в качестве сервиса в приложение
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+
+            // Добавление модели работы с пользователями (установка требований к политике паролей)
+            builder.Services.AddIdentity<User, IdentityRole>(opts => {
+                opts.Password.RequiredLength = 5;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            // Add services to the container. ???
             builder.Services.AddControllersWithViews();
+            #endregion
 
+            // Создаем WebApplication
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -24,7 +51,9 @@ namespace SocialNetwork
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication(); //Добавление аутентификации
+            app.UseAuthorization(); //Добавление авторизации
+
 
             app.MapControllerRoute(
                 name: "default",
