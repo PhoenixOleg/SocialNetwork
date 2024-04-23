fc1f3f6030acee1bf9b59b6c8aa9562ca71833bb
PhoenixOleg <metall_2000@mail.ru>
2024-04-24 01:01:37 +0300

    Добавления using на UserFromModel

--- SocialNetwork/Controllers/Account/AccountManagerController.cs
+++ SocialNetwork/Controllers/Account/AccountManagerController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Extentions;
 using SocialNetwork.Models.Users;
 using SocialNetwork.ViewModels.Account;
 
