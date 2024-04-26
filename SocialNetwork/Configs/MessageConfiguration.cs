b5cdd18374036bc57bec350eb90f508913995077
PhoenixOleg <metall_2000@mail.ru>
2024-04-27 00:28:14 +0300

    Задание 35.5.1 Конфиг EF для Message

---
+++ SocialNetwork/Configs/MessageConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialNetwork.Models.Users;
+
+namespace SocialNetwork.Configs
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Messages").HasKey(p => p.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+        }
+    }
+}
